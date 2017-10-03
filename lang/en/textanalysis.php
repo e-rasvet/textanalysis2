eefc1b7b0348706206cca368daa1a049d5bbacd0
Miraslav <dyhanie.istiny@gmail.com>
2017-10-03 12:15:36 +0500

    Create textanalysis.php

---
+++ lang/en/textanalysis.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+$string['textanalysis'] = 'textanalysis';
+$string['modulename'] = 'textanalysis';
+$string['pluginname'] = 'textanalysis';
+$string['modulenameplural'] = 'textanalysisS';
+$string['intro'] = 'Intro';
+$string['pluginadministration'] = 'textanalysis';
+$string['config_textcompare'] = 'Text compare size (4-10)';
+$string['config_textcompare_descr'] = '';
+$string['config_textword'] = 'Text word size (0-3)';
+$string['config_textword_descr'] = '';
+
+
+?>
